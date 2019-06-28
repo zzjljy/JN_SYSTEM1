eaaf58a778c1bd84f6bb9d8b49307a40520e5087
95768 <957687456@qq.com>
2019-06-28 11:09:09 +0800

    tile瓦片api

--- JN_PL_system/JN_api_1_0/tile.py
+++ JN_PL_system/JN_api_1_0/tile.py
@@ -1,16 +1,16 @@
-from flask import Blueprint, request, jsonify, Response, send_file
+from flask import Blueprint, jsonify, make_response
 from JN_PL_system.utils.response_code import RET
-from ..models import YJGKQ, DL, DLHDM, DY, GKQXZ, GX, KG, ZJ
-import io
+from ..models import YJGKQ, DL, DLHDM, DY, GKQXZ, GX, KG, ZJ, YX
 
 
 tile = Blueprint('tile', __name__)
 
 
-@tile.route('JN/JNYX/MapServer/tile/<int:x>/<int:y>/<int:z>/', methods=['GET'])
+@tile.route('JN/JNYX/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
 def get_jnyx_tile(x, y, z):
     '''
-    请求津南影像瓦片数据
+    请求津南影像瓦片数据,
+    png图片
     :param x:
     :param y:
     :param z:
@@ -18,20 +18,26 @@ def get_jnyx_tile(x, y, z):
     '''
     if not(x, y, z):
         return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
-
     try:
-        pass
+        dl_tile = YX.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
     except Exception as e:
-        pass
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
 
 
-@tile.route('JN/JNDL/MapServer/tile/<int:x>/<int:y>/<int:z>/')
+@tile.route('/JN/JNDL/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
 def get_jndl_tile(x, y, z):
     '''
     请求津南道路瓦片数据
+    png图片
     :param x:
     :param y:
-    :param z:
+    :param z:level
     :return:
     '''
     if not(x, y, z):
@@ -39,9 +45,178 @@ def get_jndl_tile(x, y, z):
     try:
         dl_tile = DL.objects(x=x, y=y, z=z).first()
         image = dl_tile['image']
-        print(image)
-        return jsonify(errorno=RET.OK, errmsg='成功', data=dl_tile)
-        # data = Response(image, minetype="image/jpg")
-        # return send_file(io.BytesIO(image.read()), attachment_filename='tile.jpg', mimetype='image/jpg')
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
     except Exception as e:
-        print('e')
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNDLHDM/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jndlhdm_tile(x, y, z):
+    '''
+    请求津南道路横断面瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = DLHDM.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNDY/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jndy_tile(x, y, z):
+    '''
+    请求津南单元瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = DY.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNGKQXZ/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jngkqxz_tile(x, y, z):
+    '''
+    请求津南管控区现状瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = GKQXZ.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNGX/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jngx_tile(x, y, z):
+    '''
+    请求津南管管线瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = GX.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNKG/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jnkg_tile(x, y, z):
+    '''
+    请求津南控规瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = KG.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNYJGKQ/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jnyjgkq_tile(x, y, z):
+    '''
+    请求津南一级管控区瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = YJGKQ.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        # print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
+
+
+@tile.route('/JN/JNZJ/MapServer/tile/<int:z>/<int:x>/<int:y>/', methods=['GET'])
+def get_jnzj_tile(x, y, z):
+    '''
+    请求津南镇界瓦片数据
+    png图片
+    :param x:
+    :param y:
+    :param z:level
+    :return:
+    '''
+    if not(x, y, z):
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    try:
+        dl_tile = ZJ.objects(x=x, y=y, z=z).first()
+        image = dl_tile['image']
+        print(image)
+        response = make_response(image)
+        response.headers['Content-Type'] = 'image/png'
+        return response
+    except Exception as e:
+        print(e)
+        return jsonify(errorno=RET.DBERR, errmsg='查询数据库错误')
